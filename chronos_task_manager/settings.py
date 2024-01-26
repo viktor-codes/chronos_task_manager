cd70a844d8b7c474ec39a204d78feefccb9f09f7
Viktor Rura <ruravictor@gmail.com>
2024-01-26 22:52:23 +0000

    Updated ALLOWED_HOSTS

--- chronos_task_manager/settings.py
+++ chronos_task_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOST")]
 
 
 # Application definition
